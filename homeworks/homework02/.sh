c740ec0d0417ff31d1013d04af3851ef57531279
STomlin3 <stomlin3@uw.edu>
2022-10-20 20:16:47 +0000

    1f778c4

---
+++ homeworks/homework02/.sh
@@ -0,0 +1,29 @@
+homework02_problem01
+#!/bin/bash
+FILENAME=${question01}
+echo my name is $user
+my name is and my home directory is The contents of the tfcb_2022/lectures/lecture04/ directory are README.md homeworks lectures reference.md software tfcb-homework02
+echo my name is $user and my home directory is $home The contents of the tfcb_2022/lectures/lecture04/ directory are $(ls) > question01.txt
+mkdir homework02
+mv question01.txt homework02
+
+problem02
+root@29d6a58f4937:/workspaces/tfcb_2022/homeworks/homework02# 
+#!/bin/bash
+mkdir question02
+cd question02
+for i in {1..25} 
+do echo protocol_$i.txt > protocol_$i.txt
+done
+
+ammend above: 
+#!/bin/bash
+mkdir question02
+cd question02
+orks/homework02/question02# 
+for i in {1..25}
+> do echo protocol_504.txt > protocol_569.txt > protocol_231.txt > protocol_717.txt > protocol_986.txt > protocol_528.txt > protocol_759.txt > protocol_654.txt > protocol_751.txt > protocol_262.txt > protocol_889.txt > protocol_281.txt > protocol_186.txt > protocol_108.txt > protocol_388.txt > protocol_374.txt > protocol_148.txt > protocol_716.txt > protocol_637.txt > protocol_550.txt > protocol_382.txt > protocol_797.txt > protocol_477.txt > protocol_202.txt > protocol_699.txt
+> done
+
+
+
